4ff3c5d0d51805e1bcdbb879ab7fb0fcf53c2b3b
Florence-Paloma <f.palomagaspar@gmail.com>
2021-08-11 00:54:44 +0000

    merge the changes

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import Footer from "./footer.jsx";
+import Cards from "./cards.jsx";
 //include images into your bundle
 
 //create your first component
 export function Home() {
-	return <Footer />;
+	return (
+		<>
+			<Cards />
+			<Footer />
+		</>
+	);
 }
